29a45a420a5d4a05d1b92aeb8fa5adf0609cd5f1
s23210 <73021331+s23210@users.noreply.github.com>
2022-06-18 11:51:23 +0200

    Initial commit

---
+++ src/main/java/pl/pjatk/jazs23210nbp/JazS23210NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jazs23210nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS23210NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS23210NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/jazs23210nbp/JazS23210NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jazs23210nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS23210NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
